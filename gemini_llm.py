9e8bd20408a340df6a74820464f77810507b7233
Abcshreyas <165556026+Abcshreyas@users.noreply.github.com>
2025-05-20 15:15:54 +0530

    Update gemini_llm.py

--- gemini_llm.py
+++ gemini_llm.py
@@ -1,22 +1,25 @@
 from langchain.llms.base import LLM
 from typing import Optional, List
-import os 
+from pydantic import PrivateAttr
+import os
 import google.generativeai as genai
 
 class GeminiLLM(LLM):
-    model: str = 'gemini-pro'
+    model: str = 'gemini-1.5-flash'
     temperature: float = 0.7
 
-def __init__(self, api_key: str, **kwargs):
-    super().__init__(**kwargs)
-    os.environ["GOOGLE_API_KEY"] = api_key
-    genai.configure(api_key=api_key)
-    self.client = genai.GenerativeModel(self.model)
+    _client: genai.GenerativeModel = PrivateAttr()
 
-@property
-def _llm_type(self) -> str:
-    return "gemini"
+    def __init__(self, api_key: str, **kwargs):
+        super().__init__(**kwargs)
+        os.environ["GOOGLE_API_KEY"] = api_key
+        genai.configure(api_key=api_key)
+        self._client = genai.GenerativeModel(self.model)
 
-def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
-    respnse = self.client.generate_content(prompt)
-    return response.text
+    @property
+    def _llm_type(self) -> str:
+        return "gemini"
+
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        response = self._client.generate_content(prompt)
+        return response.text
